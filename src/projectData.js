963b2a6435915bb4deac00926687c4b470b40514
NikEmman <121699424+NikEmman@users.noreply.github.com>
2025-01-29 12:53:55 +0200

    Fix assets location

--- src/projectData.js
+++ src/projectData.js
@@ -12,7 +12,7 @@ const projectData = [
     description:
       "A social media platform where you can follow people and see their posts",
     tech: "Ruby, Rails",
-    imgUrl: "localPath",
+    imgUrl: "",
     github: "https://github.com/NikEmman/my_circle",
   },
   {
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://nikemman.github.io/",
   optimizeDeps: {
     include: ["react-markdown"],
   },
