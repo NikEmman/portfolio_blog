78a01df1eadedcf94a008aeee01e7e7d60abe1f7
NikEmman <121699424+NikEmman@users.noreply.github.com>
2025-05-23 13:29:51 +0300

    Trim projects, fix broken link

--- src/projectData.js
+++ src/projectData.js
@@ -10,26 +10,6 @@ const projectData = [
     github: "https://github.com/NikEmman/vaxyp",
     live: "https://nikemman.github.io/vaxyp/",
   },
-  {
-    title: "Console Chess",
-    description: "A chess game for consoles",
-    tech: "Ruby",
-    imgUrl: "chess_1.png",
-    largeDescription:
-      "This is a two player game. After reading the instructions on how to play, each player inputs a name, and then play in turn. The game uses standard chess rules. Players have the ability to save/load a game, admit defeat and end the game or propose a draw to the opponent. During a game, the taken pieces are also displayed",
-    github: "https://github.com/NikEmman/Chess",
-    live: "https://replit.com/@epistimon2004/Chess",
-  },
-  {
-    title: "My circle",
-    description:
-      "A social media platform where you can follow people and see their posts",
-    tech: "Ruby, Rails, TailwindCSS",
-    imgUrl: "my_circle.png",
-    largeDescription:
-      "My circle is a social media platform. Users must create an account first, and then create their profile, including an avatar picture. On their home page they can write posts, comment on them, and like/dislike posts and comments. Can also find a list of users and sent follow requests. When the request is accepted, the user can also see posts on his home page of the followed users, in chronological order.",
-    github: "https://github.com/NikEmman/my_circle",
-  },
 
   {
     title: "Messenger",
@@ -48,7 +28,7 @@ const projectData = [
     largeDescription:
       "A browser version of the famous board game. It is made using only html/css and vanilla JavaScript. Can be a single player or two player game, if a Player 2 name is also given. Then each player is asked in turn to place 5 ships on their boards. Then game begins! If single player, the user can keep attacking the enemy board (right-side) and watch the AI hits on his/her own board(left-side). If it's a 2 player game, after each attack the player hides his board so the enemy won't peak, and then passes the screen to the enemy so he/she can attack. Again, each player is shown a friendly and enemy board. Game ends when all ships of a player are sunk!",
     github: "https://github.com/NikEmman/battleship",
-    live: "https://nikemman.github.io/battleship/c",
+    live: "https://nikemman.github.io/battleship",
   },
 ];
 export default projectData;
